79571b7f856cb63d3d839c7308ab26ae50b37858
yogesh9211 <yogeshtonpe999@gmail.com>
2022-04-27 04:31:31 +0000

    Added Changes

---
+++ Final Assesment All Phase/Phase-1-Assessment_Project/src/com/project/locker/Main.java
@@ -0,0 +1,12 @@
+package com.project.locker;
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "/home/yogeshtonpempha/temp2/"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+}
